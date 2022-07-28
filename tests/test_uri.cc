22a79683eb2e9dd133ce49ea18589290f1bd9c75
haohao-qw <dawsonqw@163.com>
2022-07-28 21:04:02 +0800

    728

--- tests/test_uri.cc
+++
@@ -1,8 +0,0 @@
-#include "LT.h"
-#include <iostream>
-
-int main(int argc, char * argv[]) {
-    auto uri = LT::Uri::Create("http://a:b@host.com:8080/p/a/t/h?query=string#hash");
-    std::cout << uri->toString() << std::endl;
-    return 0;
-}
