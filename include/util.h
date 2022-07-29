f8d80ab3b4f5cb8a342fa939008d2ffd01118ec5
WQD <71950898+haohao-qw@users.noreply.github.com>
2022-07-29 16:33:07 +0800

    Create util.h
    729

---
+++ include/util.h
@@ -0,0 +1,73 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <sys/types.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <cxxabi.h> // for abi::__cxa_demangle()
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace LT {
+
+/**
+ * @brief 获取线程id
+ * @note 这里不要把pid_t和pthread_t混淆，关于它们之的区别可参考gettid(2)
+ */
+pid_t GetThreadId();
+
+/**
+ * @brief 获取协程id
+ * @todo 桩函数，暂时返回0，等协程模块完善后再返回实际值
+ */
+uint64_t GetFiberId();
+
+/**
+ * @brief 获取当前启动的毫秒数，参考clock_gettime(2)，使用CLOCK_MONOTONIC_RAW
+ */
+uint64_t GetElapsedMS();
+
+/**
+ * @brief 获取线程名称，参考pthread_getname_np(3)
+ */
+std::string GetThreadName();
+
+/**
+ * @brief 设置线程名称，参考pthread_setname_np(3)
+ * @note 线程名称不能超过16字节，包括结尾的'\0'字符
+ */
+void SetThreadName(const std::string &name);
+
+/**
+ * @brief 获取当前的调用栈
+ * @param[out] bt 保存调用栈
+ * @param[in] size 最多返回层数
+ * @param[in] skip 跳过栈顶的层数
+ */
+void Backtrace(std::vector<std::string> &bt, int size = 64, int skip = 1);
+
+/**
+ * @brief 获取当前栈信息的字符串
+ * @param[in] size 栈的最大层数
+ * @param[in] skip 跳过栈顶的层数
+ * @param[in] prefix 栈信息前输出的内容
+ */
+std::string BacktraceToString(int size = 64, int skip = 2, const std::string &prefix = "");
+
+/**
+ * @brief 获取当前时间的毫秒
+ */
+uint64_t GetCurrentMS();
+
+/**
+ * @brief 获取当前时间的微秒
+ */
+uint64_t GetCurrentUS();
+
+} // namespace LT
+
+/**
+ * brief 删除文件 
+ * */
+bool Unlink(const std::string &filename, bool exist = false);
+#endif // 
