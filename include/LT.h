0e6d45a9e3b598f425dc0f7a25e4765c8082dac0
WQD <71950898+haohao-qw@users.noreply.github.com>
2022-07-29 16:31:11 +0800

    Update LT.h
    729

--- include/LT.h
+++ include/LT.h
@@ -1,14 +1,12 @@
-#ifndef __LT_SYLAR_H__
-#define __LT_SYLAR_H__
+#ifndef __LT_H__
+#define __LT_H__
 
-#include "log.h"
 #include "util.h"
+#include "log.h"
 #include "singleton.h"
 #include "mutex.h"
 #include "noncopyable.h"
 #include "macro.h"
-#include "env.h"
-#include "config.h"
 #include "thread.h"
 #include "fiber.h"
 #include "scheduler.h"
@@ -18,14 +16,5 @@
 #include "Endian.h"
 #include "address.h"
 #include "socket.h"
-#include "bytearray.h"
 #include "tcp_server.h"
-#include "uri.h"
-#include "http/Http.h"
-#include "http/Http_parser.h"
-#include "http/Http_session.h"
-#include "http/servlet.h"
-#include "http/Http_server.h"
-#include "http/Http_connection.h"
-#include "daemon.h"
 #endif
